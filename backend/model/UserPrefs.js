0ab973bef3d89f3f85ec66d00804b1fc613106ae
nancymz <nancyzacharias@sierrasystems.com>
2021-03-10 14:25:43 -0800

    Subscription prefs model

--- backend/model/UserPrefs.js
+++ backend/model/UserPrefs.js
@@ -1,1 +1,34 @@
 
+const mongoose = require('mongoose')
+mongoose.Promise=global.Promise
+
+const dataSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+       // unique: true,
+    },
+    isRoot:{
+        type:Boolean
+    },
+    category:{
+        type:String
+    },
+    isParent:{
+        type: Boolean,
+      //  required: true,
+    },
+    hasChildren:{
+        type:Boolean,
+        required: true,
+    },
+   children:
+           [this] 
+    ,
+    price:{
+        type: Number
+    }
+})
+let UserPrefs = mongoose.model('user_prefs',dataSchema)
+
+module.exports = UserPrefs
