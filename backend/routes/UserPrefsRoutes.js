b932bb76b53163ebe74b222709e928c0e593fb8b
nancymz <nancyzacharias@sierrasystems.com>
2021-03-10 14:27:18 -0800

    Routes to APIs

--- backend/routes/UserPrefsRoutes.js
+++ backend/routes/UserPrefsRoutes.js
@@ -1,1 +1,23 @@
 
+const mongoose = require('mongoose')
+UserPrefs = mongoose.model('user_prefs')
+
+const express = require('express');
+const router = express.Router()
+
+const userPrefsController = require('../controller/UserPrefsController')
+router.get('/',userPrefsController.baseRoute)
+
+router.get('/get',userPrefsController.getUserPrefs)
+
+router.post('/create',userPrefsController.createUserPrefs)
+
+router.get('/getEach:id', userPrefsController.getEachUserPrefs);
+
+router.put('/userPref/:id/update', userPrefsController.updateUserPref);
+
+// delete
+router.delete('/delete/:name', userPrefsController.deleteUserPref);
+
+
+module.exports = router
